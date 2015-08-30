7eeca5b1b30ed9bffb6e1282eaf5c63416664b7c
e.n.shirokov <e.n.shirokov@gmail.com>
2015-08-30 21:48:31 +0300

    v3

--- tree/main.cpp
+++ tree/main.cpp
@@ -18,13 +18,14 @@ public:
 
     };
 
-    int level;
+    int level; // уровень (нужно только для вывода на экран)
     int left; // левое число
     int right; // правое число
     Node *parent; // родиель
     vector<Node *> children; // потомки
 };
 
+namespace Tree {
 // поиск ветки по номеру (в упорядоченном дереве!)
 Node* find(int pos, Node *node)
 {
@@ -33,7 +34,7 @@ Node* find(int pos, Node *node)
 
         if (pos == node->left || pos == node->right) {
             // если искомый номер совпадает с левым номером текущего узла
-            // если искомый номер совпадает с правым номером текущего узла
+            // или если искомый номер совпадает с правым номером текущего узла
 
             return node;
 
@@ -54,60 +55,120 @@ Node* find(int pos, Node *node)
 
 }
 
+// обновление дерева вниз от node исключая ветки меньшие чем branch
 int update(int branch, int num, Node *node)
 {
 
     node->left = num;
-    node->level = (node->parent != 0) ? (node->parent->level + 1) : 0;
+    node->level = (node->parent != 0) ? (node->parent->level + 1) : 0; // уровень (только для отображения!)
 
 
-    if(!node->children.empty()){
+    if (!node->children.empty()) {
+        // если есть потомки, то рекурсивно вызываем upadte() и для них
+
         for (int i = 0; i < node->children.size(); i++) {
-            int forOther = (i == 0) ? (node->left + 1) : node->children[i - 1]->right + 1;
 
-            if(i >= branch)
+            int forOther = (i == 0) ? (node->left + 1) : node->children[i - 1]->right + 1; // номер последовательнсти для потомка
+
+            if(i >= branch) {
+                // обновляем только нужные ветки (которые больше заданного ограничения branch)
+                // и берем значение right у крайней ветки
+
                 node->right = update(0, forOther, node->children[i]);
-            else
+            }
+            else {
+                // или просто берем значение right у крайней ветки
+
                 node->right = node->children[i]->right + 1;
+            }
         }
     }
-    else
+    else {
+        // если потомков нет, то right = left + 1
+
         node->right = node->left + 1;
 
-    return node->right + 1;
+    }
+
+    return node->right + 1; // возвращаем для родителя его правый номер
 }
 
+// поиск номера ветки в векторе потомков (можно заменить добавлением поля в структуру Node  сожержащую номер ветки)
+int findBranch(Node *node)
+{
+    if(node->parent == 0)
+        return -1;
+
+    Node *parent =  node->parent;
+    auto iter = std::find(parent->children.begin(), parent->children.end(), node);
+    return parent->children.end() - iter - 1; // переходим к дургой ветке
+}
+
+// поднимаемся на уровень выше с обновлением потомков (не всех!)
 void levelUp(int num, Node *node)
 {
     if(node->parent == 0)
         return;
 
-    Node *parent =  node->parent;
-    auto iter = std::find(parent->children.begin(), parent->children.end(), node);
-    int branch = parent->children.end() - iter - 1; // переходим к дургой ветке
+    // Пиоиск номера ветки (можно заменить добавлением поля в структуру Node  сожержащую номер ветки)
+    int branch = findBranch(node);
 
-    update(branch, parent->left, parent);
+    Node *parent = node->parent;
 
-    levelUp(parent->right, parent);
+    update(branch, parent->left, parent); // обновляем ветки родительского узла (исключая уже измененные (т.е < branch))
+
+    levelUp(parent->right, parent); // рекурсивно переходим на уровень вверх
 }
 
-void insert(int pos, Node *node, Node *parent)
+// вставка ветки node в parent на место pos
+void insert(int pos, Node *node, Node *root)
 {
-    Node *p = find(pos, parent);
-
-    node->parent = p;
-
+    Node *p = find(pos, root); // поиск места вставки
 
     if(pos == p->left){
+        // если место вставки найдено - дабавляем ветку node в потомки и обновляем ее полностью (рекурсивно)
+
+        node->parent = p; // назначаем родителя новому узлу
         p->children.push_back(node);
         update(p->children.size() - 1, p->left, p);
+
+        levelUp(p->right, p); // поднимаемся на уровень выше, и обновляем все ветки справа (рекурсивно)
     }
 
-    levelUp(p->right, p);
+
 
 
 }
 
+// удаление ветки (перемещение в node) из parent на месте pos (включая узел с pos)
+void remove(int pos, Node *node, Node *root)
+{
+    Node *p = find(pos, root); // поиск места удаления
+
+    if(pos == p->left){
+        node = p;
+
+        if(p->parent != 0) {
+            int branch = findBranch(p);
+            int forOther = p->parent->left;
+            Node *parent = p->parent;
+
+            parent->children.erase(find(parent->children.begin(), parent->children.end(), p));
+            update(branch, forOther, parent);
+            levelUp(parent->right, parent); // поднимаемся на уровень выше, и обновляем все ветки справа (рекурсивно)
+
+            update(0, 1, node); // обновляем новое дерево (часть старого)
+
+        }
+
+    }
+
+
+
+
+}
+
+// вывод информации об узлах на экран
 void print(Node *node)
 {
     if(node == 0)
@@ -121,30 +182,41 @@ void print(Node *node)
 
 }
 
+} // namecpace Tree
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     Node *root = new Node;
-    insert(1, new Node, root);
-    insert(2, new Node, root);
-    insert(1, new Node, root);
+    Tree::insert(1, new Node, root);
+    Tree::insert(2, new Node, root);
+    Tree::insert(1, new Node, root);
     //insert(2, new Node, root);
 
-    print(root);
+    Tree::print(root);
 
     qDebug() << "---------------------";
 
     Node *root2 = new Node;
-    insert(1, new Node, root2);
-    insert(1, new Node, root2);
+    Tree::insert(1, new Node, root2);
+    Tree::insert(1, new Node, root2);
 
-    print(root2);
+    Tree::print(root2);
 
     qDebug() << "---------------------";
 
-    insert(3, root2, root);
-    print(root);
+    Tree::insert(3, root2, root);
+    Tree::print(root);
+
+    Node *root3 = nullptr;
+    Tree::remove(4, root2, root);
+
+    qDebug() << "Root ----------";
+    Tree::print(root);
+    qDebug() << "Root2 ---------";
+    Tree::print(root2);
+
 
     return a.exec();
 }
